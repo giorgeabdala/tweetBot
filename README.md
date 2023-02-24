c86824e20c4b58ae0c2be17e2edef12e1c47a393
Giorge Abdala <giorgeabdala@gmail.com>
2023-02-23 22:38:27 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# tweetBot
-simple Bot for like and retweete.
+# 🕊 Twitter Engagement Bot
+
+A bot that automatically likes tweets that contain certain keywords, replies to tweets, and replies random Gm and Gn to tweets and much more...
+
+
+
+## 🧪 Setup
+ 
+1. Clone this repository
+2. Install node.js v16.14.1 (https://nodejs.org/download/release/v16.14.1/)
+3. run `npm install` in the directory
+4. run `npm start` to start the bot
+
+
+## 📐 Configuration
+
+The bot is configured using config.json:
+
+* `username` - The username of the account to use (optional)
+* `password` - The password of the account to use (optional)
+* `search` - An array of keywords to search for'
+* `action` - An array of actions to perform
+* `cycles` - The number of times to perform the actions
+
+
+## ✨ Features
+
+* Automatically like tweets that contain certain keywords
+* Automatically reply random Gm and Gn to tweets
+* Automatically reply to tweets that contain certain keywords
+* Automatically follow users that tweet certain keywords
+* Automatically await 3 hours before doing the next action
---
+++ index.js
@@ -0,0 +1,135 @@
+const inquirer = require('inquirer')
+const { logError, logSuccess } = require('./src/utils/chalk')
+const TwitterBot = require('./src/TwitterBot')
+
+const config = require('./config.json')
+
+let props = {
+    action: config.action,
+    cycles: config.cycles,
+    search: config.search,
+    username: config.username,
+   password: config.password
+}
+
+async function createBot()  {
+   const bot = new TwitterBot()
+   await bot.init()
+   const username = props.username
+   const password = props.password
+   // Login to Twitter
+   await bot.login(username, password)
+   return bot
+
+}
+
+async function startBot() {
+   try {
+      const bot = await createBot()
+      const cycles = props.cycles
+
+      // Save storage state to "storage.json"
+      await bot.context.storageState({path: 'storage.json'})
+
+      if (props.search) {
+         await bot.search(props.search)
+      }
+
+      // Perform the action
+      switch (props.action) {
+         case 'gm':
+            if (!props.search) {
+               await bot.search('Gm')
+            }
+            await bot.performMultipleTweetActions(cycles, {
+               like: true,
+               reply: true,
+               getReply: () => bot.getRandomGm(),
+            })
+            break
+         case 'gn':
+            if (!props.search) {
+               await bot.search('Gn')
+            }
+            await bot.performMultipleTweetActions(cycles, {
+               like: true,
+               reply: true,
+               getReply: () => bot.getRandomGn(),
+            })
+            break
+         case 'like_tweets':
+            await bot.performMultipleTweetActions(cycles, {
+               like: true,
+            })
+            break
+         case 'follow':
+            await bot.performMultipleTweetActions(cycles, {
+                like: true,
+               reply: false,
+               follow: true
+            })
+            break
+         case 'reply_tweets':
+            await bot.performMultipleTweetActions(cycles, {
+               like: true,
+               reply: true,
+               getReply: () => props.reply,
+            })
+            break
+         default:
+            logError('Invalid action')
+            process.exit(1)
+            break
+      }
+
+      logSuccess(`Done! ${cycles} actions performed`)
+
+      // Close the browser
+      await bot.close()
+
+
+   } catch (error) {
+      if (error.isTtyError) {
+         // Prompt couldn't be rendered in the current environment
+         logError(`Prompt couldn't be rendered in the current environment`)
+      } else {
+         // Something else when wrong
+         logError(`Something went wrong: ${error}`)
+      }
+
+
+   }
+}
+
+async function start() {
+   try {
+      props.search = config.search[Math.floor(Math.random() * config.search.length)]
+      props.cycle = Math.floor(Math.random() * config.cycles + 1)
+      props.action = config.action[Math.floor(Math.random() * config.action.length)]
+      run = Math.random() >= 0.5;
+
+      if (run) {
+         await startBot()
+      }
+        await awaitAndStart()
+   }
+    catch (error) {
+      console.log(error)
+       await awaitAndStart()
+    }
+}
+
+async function awaitAndStart() {
+   console.log('sleeping for 3 hours')
+    setTimeout(async function() {
+         await start();
+      },
+      10800000);
+}
+
+
+start().then(r =>  console.log(r))
+
+
+
+
